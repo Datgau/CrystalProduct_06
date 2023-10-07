08151d29187f8d0118d68351809faf8618646cfa
datgau <datbeo1204@gmail.com>
2023-10-07 16:57:15 +0700

    updateFuncition

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const btns = document.querySelectorAll("[data-target]");
 const close_modals = document.querySelectorAll(".close-modal");
 const overlay = document.getElementById("overlay");
 
+var cart = [];
+var productElements = document.querySelectorAll('.product-item');
+
 function filter() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('searchInput');
@@ -32,6 +35,25 @@ function hideProductDetails(productId) {
         productDetails.style.display = "none";
     }
 }
+function sortList() {
+    var productList = document.getElementById("product-list");
+    var productItems = productList.querySelectorAll("li"); 
+   var productArray = Array.from(productItems); 
+    productArray.sort(function(a, b) {
+        var priceA = parseFloat(a.querySelector(".product-price").textContent.replace("$", ""));
+        var priceB = parseFloat(b.querySelector(".product-price").textContent.replace("$", ""));
+        return priceA - priceB;
+    });
+
+
+    for (var i = 0; i < productItems.length; i++) {
+        productList.removeChild(productItems[i]);
+    }
+
+    for (var j = 0; j < productArray.length; j++) {
+        productList.appendChild(productArray[j]);
+    }
+}
 function closeProductDetails(productId) {
     var productDetails = document.getElementById(productId + "-details");
     if (productDetails) {
@@ -39,32 +61,39 @@ function closeProductDetails(productId) {
     }
 }
 
-function luuDulieuGioHangVaoLocalStorage() {
-    if (typeof Storage !== "undefined") {
-      try {
-        localStorage.setItem("cart", JSON.stringify(cart));
-      } catch (error) {
-        console.error(`Lỗi khi lưu dữ liệu vào localStorage: ${error}`);
-      }
-    } else {
-      console.error("Trình duyệt của bạn không hỗ trợ localStorage.");
-    }
+if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+  } else {
+    cart = [];
   }
+  
+  function luuDulieuGioHangVaoLocalStorage() {
+      if (typeof Storage !== "undefined") {
+          try {
+              localStorage.setItem("cart", JSON.stringify(cart));
+          } catch (error) {
+              console.error(`Lỗi khi lưu dữ liệu vào localStorage: ${error}`);
+          }
+      } else {
+          console.error("Trình duyệt của bạn không hỗ trợ localStorage.");
+      }
+  }
+  
+  function addToCart(productId, productName) {
+      var Item = document.getElementById(productId);
+      var productThumb = Item.querySelector('.product-thumb');
+      var productName = Item.querySelector('.product-name').textContent;
+      var productPrice = Item.querySelector('.product-price').textContent;
+  
+      cart.push({ id: productId, name: productName, price: productPrice });
+      luuDulieuGioHangVaoLocalStorage();
+  
+      alert("Product has been added to the shopping cart successfully !!!\nName: " + productName + " \nPrice: " + productPrice + "");
+  }
+  
 
-var cart = [];
 
-var productElements = document.querySelectorAll('.product-item');
 
-function addToCart(productId, productName) {
-    var Item = document.getElementById(productId);
-    var productThumb = Item.querySelector('.product-thumb');
-    var productName = Item.querySelector('.product-name').textContent;
-    var productPrice = Item.querySelector('.product-price').textContent;
-    
-   cart.push({ id: productId, name: productName, price: productPrice,});
-   luuDulieuGioHangVaoLocalStorage();
-   alert("Product has been added to the shopping cart successfully !!!\nName: "+productName+" \nPrice: "+productPrice+"");
-}
 
 
 
